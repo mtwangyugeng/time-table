7e962160b56eed00e1b5fbbdbc826042dcaa94c1
Simon Wang <mtwangyugeng@gmail.com>
2022-05-26 21:33:53 -0400

    cursor stamp

--- src/lib/TimeTable/Stamps/TestCursor.svelte
+++ src/lib/TimeTable/Stamps/TestCursor.svelte
@@ -1,9 +1,41 @@
 <script>
     import { onDestroy, onMount } from "svelte";
+import LinkedInSvg from "./svg/LinkedInSVG.svelte";
     onMount(()=> {
-        document.body.style.cursor = "pointer"
+        document.body.style.cursor = "none";
     })
     onDestroy(() => {
-        document.body.style.cursor = "auto"
+        document.body.style.cursor = "auto";
     })
+
+    let pX;
+    let pY;
+    const handleMove = (e) => {
+        if (e.type === "mousemove"){
+            pX = e.clientX;
+            pY = e.clientY;
+        } else {
+            pX = e.touches[0].clientX;
+            pY = e.touches[0].clientY;
+        }
+    }
 </script>
+
+<svelte:window on:mousemove={handleMove} on:touchmove={handleMove}/>
+
+<span style="top: {pY}px; left: {pX}px;">
+    <LinkedInSvg />
+</span>
+
+<style>
+    span {
+        position: fixed;
+        pointer-events: none;
+    }
+
+    span :global(svg) {
+        fill: rgb(46, 124, 197);
+        width: 30px;
+        height: 30px;
+    }
+</style>
